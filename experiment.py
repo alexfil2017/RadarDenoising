f3de471b6c8ef7b04e977e893bc15cefef031b5b
Sofiane Horache <shorache@tsicluster02.enst.fr>
2018-03-28 19:36:50 +0200

    remove the outputs

--- experiment.py
+++ experiment.py
@@ -42,7 +42,7 @@ print(res)
 file.write(res+'\n')
 utils.displayRSO(np.sqrt(img_gt),is_display=False, path='{0}/ground_truth.png'.format(args.out_dir))
 utils.displayRSO(np.sqrt(img_speckle),is_display=False, path='{0}/noisy_image_L{1}.png'.format(args.out_dir, args.L))
-utils.displayRSO(np.sqrt(img_speckle[200:400,500:700]),is_display=False,
+utils.displayRSO(np.sqrt(img_gt[200:400,500:700]),is_display=False,
                  path='{0}/noisy_image_detail_L{1}.png'.format(args.out_dir, args.L))
 
 
