25501468f8809d89bdea793bda39dc0ab749b089
Sofiane Horache <shorache@tsicluster01.enst.fr>
2018-03-29 17:15:26 +0200

    correct 3 mistakes : add coeff lambda to the energy. add the forgotten L in the newton step. add a coeff alpha to control the influence of the dataterm

--- ADMM.py
+++ ADMM.py
@@ -2,11 +2,12 @@ import GaussianDenoiser as gd
 import numpy as np
 import scipy
 
-def ADMM(im, beta,lamb, niter, CNNprior = None, verbose=False):
+def ADMM(im, beta,lamb, niter, L=1, CNNprior = None, verbose=False):
     "computes the denoising of im (log of intensity) with ADMM"
 
 
     #INITIALIZE PARAMETERS
+    alpha=1  # ADDED : coefficient in the data term
     x = im.astype(float)
     z  = im.astype(float) #np.zeros(im.shape)
 
@@ -15,6 +16,9 @@ def ADMM(im, beta,lamb, niter, CNNprior = None, verbose=False):
     for k in range(niter):
         #compute the proximal of the regularisation term
         if (CNNprior is not None):
+            
+            alpha = 1.0/lamb # it is a constant in the data term.
+            
             if(verbose):
                 print("iteration for the CNN: "+str(k))
             v=x-d
@@ -41,12 +45,13 @@ def ADMM(im, beta,lamb, niter, CNNprior = None, verbose=False):
         a = z+d
         #NEWTON STEPS
         for j in range(10):
-            xtmp -= np.divide(beta*(xtmp-a)+np.ones(im.shape)-np.exp(im-xtmp),beta*np.ones(im.shape)+np.exp(im-xtmp))
+            xtmp -= np.divide(beta*(xtmp-a)+L*alpha(np.ones(im.shape)-np.exp(im-xtmp)),
+                              beta*np.ones(im.shape)+L*alpha*np.exp(im-xtmp))
 #             print ( np.sum((xtmp-a)**2)+np.sum(xtmp)+np.sum(np.exp(im-xtmp)))
         x = xtmp
         #compute the Energy
         gradx, grady = gd.grad(x)
-        e = np.sum(x)+np.sum(np.exp(im-x))+np.sum(np.sqrt(gradx**2 + grady**2))
+        e = np.sum(x)+np.sum(np.exp(im-x))+ lamb * np.sum(np.sqrt(gradx**2 + grady**2))
         E.append(e)
 
     return x, E
